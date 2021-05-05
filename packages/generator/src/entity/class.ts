0d39013a52c20432a8a892abe9b9f6ef6d0fbaac
Jannis Rosenbaum <j.ro@dock51.de>
2021-05-05 14:32:16 +0200

    switch from entitySetName to entityTypeName for remaining types

--- packages/generator/src/entity/class.ts
+++ packages/generator/src/entity/class.ts
@@ -101,7 +101,7 @@ function navProperty(
   navProp: VdmNavigationProperty,
   service: VdmServiceMetadata
 ): PropertyDeclarationStructure {
-  const entity = service.entities.find(e => e.entitySetName === navProp.to);
+  const entity = service.entities.find(e => e.entityTypeName === navProp.to);
   if (!entity) {
     throw Error(
       `Failed to find the entity from the service: ${JSON.stringify(
